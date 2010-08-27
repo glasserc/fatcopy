ad756297d258abacf589f178bb11323052adb2a0
Ethan Glasser-Camp <ethan@betacantrips.com>
2010-08-27 18:40:30 -0400

    FAT-safety on copy-single

--- test/test_fatcopy.py
+++ test/test_fatcopy.py
@@ -9,9 +9,9 @@ except ImportError:
 class FatCopyTest(unittest.TestCase):
     def setUp(self):
         self.app = fatcopy.FatCopy("foo", "bar")
+        self.app.fs = mock.Mock()
 
     def test_copy_single_nodir(self):
-        self.app.fs = mock.Mock()
         self.app.fs.isdir.return_value = False
         self.app.fatcopy_single("a/b/c", "d/e")
 
@@ -19,9 +19,15 @@ class FatCopyTest(unittest.TestCase):
         self.assertEqual(self.app.fs.copyfile.call_args, (("a/b/c", "d/e"), {}))
 
     def test_copy_single_dir(self):
-        self.app.fs = mock.Mock()
         self.app.fs.isdir = lambda fname: fname == "d/e"
         self.app.fatcopy_single('a/b/c', 'd/e')
 
         self.assertTrue(self.app.fs.copyfile.called)
         self.assertEqual(self.app.fs.copyfile.call_args, (("a/b/c", "d/e/c"), {}))
+
+    def test_copy_single_dir_fatsafe(self):
+        self.app.fs.isdir = lambda fname: fname == "d/e"
+        self.app.fatcopy_single('a/b/c*?:', 'd/e')
+
+        self.assertTrue(self.app.fs.copyfile.called)
+        self.assertEqual(self.app.fs.copyfile.call_args, (("a/b/c*?:", "d/e/c___"), {}))
