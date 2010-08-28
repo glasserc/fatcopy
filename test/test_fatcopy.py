78c505d657bc04c4e1822a0c59a3c247bfce8ec4
Ethan Glasser-Camp <ethan@betacantrips.com>
2010-08-28 05:21:16 -0400

    More tests

--- test/test_fatcopy.py
+++ test/test_fatcopy.py
@@ -46,3 +46,19 @@ class FatCopyTest(unittest.TestCase):
                 (("New",), {}),
                 (("New/Bar_",), {})))
         self.assertTrue(self.app.fs.copyfile.call_args, ((("Foo?/Bar:/Baz", "New/Bar_/Baz"), {})))
+
+    def test_copy_single_recurse_dir(self):
+        fs = {'Foo?': ['Bar:'], 'Foo?/Bar:': ["Baz"], 'New': []}
+        self.fixture(fs)
+        self.app.fatcopy_single('Foo?', 'New')
+
+        self.assertTrue(self.app.fs.mkdir.call_args, (
+                (("New/Foo_",), {}),
+                (("New/Foo_/Bar_",), {})))
+        self.assertTrue(self.app.fs.copyfile.call_args, ((("Foo?/Bar:/Baz", "New/Foo_/Bar_/Baz"), {})))
+
+
+    def test_copy_single_dir_failure(self):
+        fs = {'Foo?': ['Bar:'], 'Foo?/Bar:': ["Baz"], 'New': None}
+        self.fixture(fs)
+        self.assertRaises(ValueError, self.app.fatcopy_single, "Foo?", "New")
